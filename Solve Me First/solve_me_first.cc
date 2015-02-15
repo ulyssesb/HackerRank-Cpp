7b2c8b1ab57653f827eb246a45fa7df7d293941e
Ulysses Bonfim <ulyssesb@gmail.com>
2015-02-15 13:22:44 -0200

    Removed xcuserdata

---
+++ Solve Me First/solve_me_first.cc
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Solve Me First
+//
+//  Created by Ulysses Bonfim on 15/2/15.
+//  Copyright (c) 2015 Ulysses Bonfim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
