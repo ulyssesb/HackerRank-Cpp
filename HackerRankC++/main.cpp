b223590b403372e9d759812808857f328fa6642c
Ulysses Bonfim <ulyssesb@gmail.com>
2015-02-15 11:55:34 -0200

    Initial commit from local git

---
+++ HackerRankC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HackerRankC++
+//
+//  Created by Ulysses Bonfim on 15/2/15.
+//  Copyright (c) 2015 Ulysses Bonfim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
