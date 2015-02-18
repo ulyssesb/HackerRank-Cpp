71b7de4ef5adec42eec2c6bba6f8d50d46916a38
Ulysses Bonfim <ulyssesb@gmail.com>
2015-02-18 09:24:43 -0200

    Algorithms :: Warmup :: Lonely Integer

--- algorithms/1_warmup/src/lonely-integer.cc
+++ algorithms/1_warmup/src/lonely-integer.cc
@@ -11,37 +11,36 @@
 #include <unordered_map>
 using namespace std;
 
-#define MAX_DIGITS 10
+typedef unordered_map<int,int> IntegerMap;
 
-vector<int> GroupDigitOccurencies( const vector<int>& integers ) {
-  vector<int> occurrencies(10);
+IntegerMap GroupIntegerOccurrencies( const vector<int>& integers ) {
+  IntegerMap occurrencies = {};
   
-  for (auto i: integers) {
+  for (auto i: integers)
     occurrencies[i]++;
-  }
+  
   return occurrencies;
 }
 
-int FindLonelyDigit( const vector<int> occurrencies){
+int FindLonelyDigit( IntegerMap occurrencies){
   int lonely_digit{0};
   
-  for ( int i=0; i < MAX_DIGITS ; i++ ){
-    if ( occurrencies[i] == 1 )
-      lonely_digit = i;
-  }
+  for ( auto integer_occurrencies: occurrencies)
+    if ( integer_occurrencies.second == 1 )
+      lonely_digit = integer_occurrencies.first;
+  
   return lonely_digit;
 }
 
 int LonelyInteger( const vector<int>& integers ) {
-  auto occurrencies = GroupDigitOccurencies( integers );
-  return FindLonelyDigit(occurrencies);
+  auto occurrencies = GroupIntegerOccurrencies( integers );
+  return FindLonelyDigit( occurrencies );
 };
 
 int main ( void ){
   
   auto num_integers=0;
   cin >> num_integers;
-  cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
   
   vector<int> integers;
   int integer;
