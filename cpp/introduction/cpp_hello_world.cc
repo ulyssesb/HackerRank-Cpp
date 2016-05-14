becc5564f73e355560d3503d1e541a3864a3b0b0
Ulysses Bonfim <ulyssesb@gmail.com>
2016-05-14 21:11:24 +0200

    Hello world in Cpp

---
+++ cpp/introduction/cpp_hello_world.cc
@@ -0,0 +1,12 @@
+/**
+* HackerRank
+* https://www.hackerrank.com/challenges/cpp-hello-world
+* Created by Ulysses Bonfim on 14/05/16
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello, World!");
+}
